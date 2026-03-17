c4d5caf1c7f7bb147f75d5b5c04b3f67c847a05a
KuznetzovAlexander <seganv02@mail.ru>
2026-03-17 22:02:36 +0300

    Task2

---
+++ Tyuiu.KuznetzovAS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KuznetzovAS.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.KuznetzovAS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Александр";
+            var res = DataService.GetMessage(name);
+
+            //Вызовем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет, Александр", res);
+        }
+    }
+}
