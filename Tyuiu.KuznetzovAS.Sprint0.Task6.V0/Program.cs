9ecb276ee122e9ca4b994bd1edcc4619eafb1eb5
KuznetzovAlexander <seganv02@mail.ru>
2026-03-18 11:25:37 +0300

    Task6

--- Tyuiu.KuznetzovAS.Sprint0.Task6.V0/Program.cs
+++ Tyuiu.KuznetzovAS.Sprint0.Task6.V0/Program.cs
@@ -7,10 +7,21 @@ using Tyuiu.KuznetzovAS.Sprint0.Task6.V0.Lib;
 
 namespace Tyuiu.KuznetzovAS.Sprint0.Task6.V0
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+            // Пример линейной структуры наъодится в библиоктеке классов в методе AdditionArray
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+
+            // Пример линейной структуры наъодится в библиоктеке классов в методе SubtractionArray
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numsArray));
+
+            // Пример линейной структуры наъодится в библиоктеке классов в методе MultiplicationArray
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
         }
     }
 }
